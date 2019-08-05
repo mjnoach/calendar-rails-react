16cc8a825e0bb59a72e56d423d7981501c7d43be
Andrzej Sienkiewicz <a.siennkiewicz@gmail.com>
2019-08-05 15:06:43 +0200

    Separate backend/frontend

---
+++ backend/app/javascript/components/Appointment.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {formatDate} from '../utils/format';
+import {Link} from 'react-router-dom';
+import $ from 'jquery';
+
+export default class Appointment extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			appointment: props.appointment
+		}
+	}
+
+	static propTypes = {
+		appointment: PropTypes.object.isRequired
+	};
+
+	static defaultProps = {
+		appointment: {}
+	}
+
+	componentDidMount() {
+		if (this.props.match) {
+			$.ajax({
+				type: "GET",
+				url: `http://localhost:3000/appointments/${this.props.match.params.id}`,
+				dataType: "JSON",
+				headers: JSON.parse(sessionStorage.getItem('user'))
+			}).done((data) => {
+				this.setState({appointment: data});
+			})
+		}
+	}
+
+	render() {
+		return (
+			<div className='appointment'>
+				<h2>Appointment</h2>
+				<Link to={`/appointments/${this.state.appointment.id}`} >
+					<h3>{this.state.appointment.title}</h3>
+				</Link>
+				<p>{formatDate(this.state.appointment.appt_time)}</p>
+				<Link to={`/appointments/${this.state.appointment.id}/edit`} >
+					Edit
+				</Link>
+			</div>
+		)
+	}
+}
---
+++ backend/app/javascript/components/Appointments.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppointmentForm from './AppointmentForm';
+import {AppointmentsList} from './AppointmentsList';
+import update from 'immutability-helper';
+import $ from 'jquery';
+
+export default class Appointments extends React.Component {
+	constructor(props, railsContext) {
+		super(props);
+		this.state = {
+			appointments: this.props.appointments
+		};
+	}
+
+	static propTypes = {
+		appointments: PropTypes.array.isRequired
+	};
+
+	static defaultProps = {
+		appointments: []
+	}
+
+	componentDidMount() {
+		if (this.props.match && sessionStorage.user) {
+			$.ajax({
+				type: "GET",
+				url: "http://localhost:3000/appointments/",
+				dataType: "JSON",
+				headers: JSON.parse(sessionStorage.getItem('user'))
+			}).done((data) => {
+				this.setState({appointments: data});
+			})
+		}
+	}
+
+	addNewAppointment = (appointment) => {
+		const appointments = update(this.state.appointments, {$push: [appointment]});
+		this.setState({appointments: appointments.sort(function(a,b) {
+			return new Date(a.appt_time) - new Date(b.appt_time);
+		})});
+	}
+
+	render() {
+		return (
+			<div>
+				<AppointmentForm handleNewAppointment={this.addNewAppointment} />
+				<AppointmentsList appointments={this.state.appointments} />
+			</div>
+		)
+	}
+}
